90f82e1bd2e8d54f9072a7897f9c3fbb031c2871
Nick Kugaevsky <nick@kugaevsky.ru>
2014-09-03 01:02:21 +0400

    Controllers: [application] check authentication for profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,21 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :error, :failure, :success, :alert
 
+  before_filter :authenticate_user!, if: -> { profile_path? }
+
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+     request.env['omniauth.origin'] ||
+     stored_location_for(resource) ||
+     root_path
+  end
+
+  private
+
+  def profile_path?
+    controller_namespace == My
+  end
+
+  def controller_namespace
+    self.class.parent
   end
 end
