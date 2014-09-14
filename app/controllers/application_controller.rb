8112402d78022acb63f32267c241116322225e43
Nick Kugaevsky <nick@kugaevsky.ru>
2014-09-14 17:07:35 +0200

    Config: set redirect to main host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,18 @@ class ApplicationController < ActionController::Base
   add_flash_types :error, :failure, :success, :alert
 
   before_action :authenticate_user!, if: -> { profile_path? }
+  before_filter :redirect_to_main_domain, if: -> { request.host != 'www.it52.info' && Rails.env.production? }
 
   def after_sign_in_path_for(resource)
      request.env['omniauth.origin'] ||
      stored_location_for(resource) ||
      root_path
+  end
+
+  protected
+
+  def redirect_to_main_domain
+    redirect_to "http://new.com#{request.request_uri}", status: :moved_permanently
   end
 
   private
