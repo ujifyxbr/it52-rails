72be408e3d48beb90237c4b71db99e7d380a8995
Nick Kugaevsky <nick@kugaevsky.ru>
2015-05-16 03:08:48 +0300

    Optimize API events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,15 +3,23 @@ module Api
     class EventsController < ApiController
       load_and_authorize_resource
 
+      before_action :prepare_model
+
       def index
         year = params[:year].present? ? params[:year].to_i : Time.now.year
-        @response_object = Event.held_in(year).decorate
+        @response_object = @model.held_in(year)
         render render_options
       end
 
       def show
-        @response_object = Event.find(params[:id]).decorate
+        @response_object = @model.find(params[:id])
         render render_options
+      end
+
+      private
+
+      def prepare_model
+        @model = Event.includes(:participants, participants: :authentications)
       end
     end
   end
