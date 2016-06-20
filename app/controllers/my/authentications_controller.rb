b7d86a71929db967c3c144655ce15ac25aef58d0
Nick Kugaevsky <nick@kugaevsky.ru>
2016-06-20 17:40:33 +0300

    Replace count with size

--- app/controllers/my/authentications_controller.rb
+++ app/controllers/my/authentications_controller.rb
@@ -2,7 +2,7 @@ class My::AuthenticationsController < ApplicationController
   before_action :set_user_and_auth
 
   def destroy
-    if @user.authentications.count == 1 && @user.email.blank?
+    if @user.authentications.size == 1 && @user.email.blank?
       flash[:danger] = t('authentications.not_destroyed')
     else
       @authentication.destroy
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -45,32 +45,3 @@ class UserSerializer < ActiveModel::Serializer
     Rails.application.routes.url_helpers.user_url(object, host: Figaro.env.mailing_host)
   end
 end
-
-
-#
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(128)      default(""), not null
-#  password_salt          :string(255)      default(""), not null
-#  created_at             :datetime
-#  updated_at             :datetime
-#  reset_password_token   :string(255)
-#  nickname               :string(255)      default("")
-#  role                   :integer
-#  first_name             :string(255)
-#  last_name              :string(255)
-#  bio                    :text
-#  avatar_image           :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :inet
-#  last_sign_in_ip        :inet
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string(255)
-#  slug                   :string(255)
-#  website                :string(255)
