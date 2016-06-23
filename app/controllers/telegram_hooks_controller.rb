1b059daca64e5ca0befb75cdb11bdf7f3ef90dc4
Nick Kugaevsky <nick@kugaevsky.ru>
2016-06-23 19:59:27 +0300

    Hanlde telegram webhook

--- app/controllers/telegram_hooks_controller.rb
+++ app/controllers/telegram_hooks_controller.rb
@@ -2,6 +2,8 @@ class TelegramHooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def process_bot_request
+    req = Telegram::Request.new bot_request_params
+    req.reply!
     render nothing: true, status: :ok
   end
 
