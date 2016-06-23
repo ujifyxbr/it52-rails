adbf336c4330f0e977ad3f62b3bca9f12230e3b9
Nick Kugaevsky <nick@kugaevsky.ru>
2016-06-23 17:16:02 +0300

    Add telegram hooks debugging

--- app/controllers/telegram_hooks_controller.rb
+++ app/controllers/telegram_hooks_controller.rb
@@ -1,7 +1,8 @@
 class TelegramHooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def process
+  def process(test = nil)
+    Rails.logger.error test.inspect
     Rails.logger.error params.inspect
     Rails.logger.error JSON.parse(params.inspect)
     render nothing: true, status: :ok
