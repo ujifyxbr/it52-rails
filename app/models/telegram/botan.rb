d89a6095f3b6e98dacd895bef381b2fb2b91def3
Nick Kugaevsky <nick@kugaevsky.ru>
2016-06-24 16:15:09 +0300

    Fix bot tracking

--- app/models/telegram/botan.rb
+++ app/models/telegram/botan.rb
@@ -1,25 +1,27 @@
-module Botan
-  require 'net/http'
-  require 'net/https'
-  require 'json'
+module Telegram
+  module Botan
+    require 'net/http'
+    require 'net/https'
+    require 'json'
 
-  URI_TEMPLATE = 'https://api.botan.io/track?token=%{token}&uid=%{uid}&name=%{name}';
+    URI_TEMPLATE = 'https://api.botan.io/track?token=%{token}&uid=%{uid}&name=%{name}';
 
-  def self.track(token, uid, message, name = 'Message')
-    begin
-      uri = URI(URI_TEMPLATE % {token: token, uid: uid, name: name})
-      puts uri
-      http = Net::HTTP.new(uri.host, uri.port)
-      body = JSON.dump(message)
+    def self.track(token, uid, message, name = 'Message')
+      begin
+        uri = URI(URI_TEMPLATE % {token: token, uid: uid, name: name})
+        puts uri
+        http = Net::HTTP.new(uri.host, uri.port)
+        body = JSON.dump(message)
 
-      req =  Net::HTTP::Post.new(uri)
-      req.add_field "Content-type", "application/json"
-      req.body = body
-      # Fetch Request
-      res = http.request(req)
-      return JSON.parse(res.body)
-    rescue StandardError => e
-      puts "HTTP Request failed (#{e.message})"
+        req =  Net::HTTP::Post.new(uri)
+        req.add_field "Content-type", "application/json"
+        req.body = body
+        # Fetch Request
+        res = http.request(req)
+        return JSON.parse(res.body)
+      rescue StandardError => e
+        puts "HTTP Request failed (#{e.message})"
+      end
     end
   end
 end
