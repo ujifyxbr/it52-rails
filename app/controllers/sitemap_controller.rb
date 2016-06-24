279a992793434e718d35d7517572286faff3b362
Nick Kugaevsky <nick@kugaevsky.ru>
2016-06-24 17:36:27 +0300

    Add sitemap

---
+++ app/controllers/sitemap_controller.rb
@@ -0,0 +1,9 @@
+class SitemapController < ApplicationController
+  respond_to :xml
+
+  def index
+    @events = Event.published
+    @users = User.all
+    @pages = HighVoltage.page_ids
+  end
+end
