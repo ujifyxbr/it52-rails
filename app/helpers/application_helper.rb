0ac21926438b6e9b442a4efe1db786ea2f4d8dd3
Nick Kugaevsky <nick@kugaevsky.ru>
2018-09-10 18:22:05 +0300

    Change logo symbol for signed in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def render_editor?
     controller.controller_name == 'events' && !['index', 'show'].include?(controller.action_name)
   end
+
+  def logo_class
+    return 'root' if user_signed_in?
+    ''
+  end
 end
