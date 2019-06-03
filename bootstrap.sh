67af718f25eaa271e8ec1277014fe887c0728e45
Nick Kugaevsky <nick@kugaevsky.ru>
2019-06-03 20:42:35 +0300

    Remove database.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Copying config files...'
+cp config/database.yml.template config/database.yml
 cp config/secrets.yml.template config/secrets.yml
 cp config/application.yml.template config/application.yml
 
