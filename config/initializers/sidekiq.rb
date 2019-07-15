234b2f34c4d5d587d3eb52040d3662f34d4ad021
Nick Kugaevsky <nick@kugaevsky.ru>
2019-07-15 10:21:12 +0300

    Dup frozen sidekiq options hash

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,9 @@ REDIS_OPTIONS = { url: ENV.fetch('REDIS_URL') { 'redis://127.0.0.1:6379' },
                   db: 1 }.freeze
 
 Sidekiq.configure_server do |config|
-  config.redis = REDIS_OPTIONS
+  config.redis = REDIS_OPTIONS.dup
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = REDIS_OPTIONS
+  config.redis = REDIS_OPTIONS.dup
 end
