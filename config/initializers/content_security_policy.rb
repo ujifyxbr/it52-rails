2035a436fdf14d797adde7c9eb6527a11f380cd6
ujifyxbr <ujifyxbr@gmail.com>
2022-08-17 14:40:38 +0300

    фикс для отображения корректных блоков информации на странице с эвентом

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,12 +7,12 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :none
+  #policy.default_src :none
   policy.font_src    :self, :https, :data, 'https://assets.it52.info', 'https://fonts.googleapis.com'
   policy.img_src     :self, :https, :data, 'https://assets.it52.info', 'https://mc.yandex.ru/'
   policy.frame_src 'https://yastatic.net', 'https://api-maps.yandex.ru/', 'https://money.yandex.ru', 'https://www.patreon.com', 'https://docs.google.com'
   policy.script_src  :self, :https, :unsafe_inline, 'https://assets.it52.info', 'https://bam.nr-data.net', 'js-agent.newrelic.com', '*.newrelic.com', 'https://mc.yandex.ru/', 'https://api-maps.yandex.ru', 'ajax.cloudflare.com'
-  policy.style_src   :self, :https, :unsafe_inline, :blob, 'https://fonts.googleapis.com', 'https://assets.it52.info'
+  #policy.style_src   :self, :https, :unsafe_inline, :blob, 'https://fonts.googleapis.com', 'https://assets.it52.info'
 
   #   # Specify URI for violation reports
   #   # policy.report_uri "/csp-violation-report-endpoint"
